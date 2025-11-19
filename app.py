6685a46c8eb28257d58b1d91acc6b9400cb23ba3
Tarun Tapan Tripathy <tapantarun6@gmail.com>
2025-11-19 19:26:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,5 +48,6 @@ def home():
     return render_template("index.html", prediction=None)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=7860, debug=True)
+
 
